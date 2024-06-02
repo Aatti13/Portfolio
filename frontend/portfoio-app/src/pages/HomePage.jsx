4d9452b38d5b60ac9d2265ec6ee90706ee9e91a3
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2024-06-03 01:11:02 +0530

    added cards and modified responsivity

--- frontend/portfoio-app/src/pages/HomePage.jsx
+++ frontend/portfoio-app/src/pages/HomePage.jsx
@@ -11,7 +11,50 @@ function HomePage(){
         <Hero />
       </section>
       <main>
+        <MainBody />
       </main>
+    </div>
+  );
+}
+
+const MainBody = ()=>{
+  const cardDetails = [
+    {
+      img: './images/ai_blog.jpg',
+      title: 'A.I and the Future',
+      description: 'What does the Artificial Inteligence have in store for the future?',
+      src: '',
+    },
+    {
+      img: './images/ai_blog.jpg',
+      title: 'What makes Web-development Fascinating?',
+      description: 'Bala is a boy',
+      src: '',
+    },
+    {
+      img: './images/ai_blog.jpg',
+      title: 'A.I and the Future',
+      description: 'What does the Artificial Inteligence have in store for the future?',
+      src: '',
+    },
+  ]
+  return(
+    <div className="blogContainer">
+      <h2 className="blogHead">A few Blogs...</h2>
+      <div className="cardContainer">
+        {cardDetails.map((cardDetail)=>{
+          return(
+           <div className="card">
+            <img src={cardDetail.img} alt="img" className="blogImg"/>
+             <h3>{cardDetail.title}</h3>
+              <p>{cardDetail.description}</p>
+              <a href={cardDetail.src}>
+                <button>View</button>
+              </a>
+           </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
